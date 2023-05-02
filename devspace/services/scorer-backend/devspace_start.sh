6651e6ca1ef3391133024c1e64dc8e1fc519b4a0
Shady Smaoui <shadysmaoui@hotmail.fr>
2023-05-02 17:50:27 -0400

    add scorer-backend service

--- devspace/services/scorer-backend/devspace_start.sh
+++ devspace/services/scorer-backend/devspace_start.sh
@@ -7,9 +7,9 @@ COLOR_RESET="\033[0m"
 
 # Print useful output for user
 echo -e "${COLOR_BLUE}
-     %########%
-     %###########%       ____                 _____
-         %#########%    |  _ \   ___ __   __ / ___/  ____    ____   ____ ___
+     %########%      
+     %###########%       ____                 _____                      
+         %#########%    |  _ \   ___ __   __ / ___/  ____    ____   ____ ___ 
          %#########%    | | | | / _ \\\\\ \ / / \___ \ |  _ \  / _  | / __// _ \\
      %#############%    | |_| |(  __/ \ V /  ____) )| |_) )( (_| |( (__(  __/
      %#############%    |____/  \___|  \_/   \____/ |  __/  \__,_| \___\\\\\___|
@@ -22,7 +22,7 @@ Welcome to your development container!
 This is how you can work with it:
 - Files will be synchronized between your local machine and this container
 - Some ports will be forwarded, so you can access this container via localhost
-- Run \`${COLOR_GREEN}go run main.go${COLOR_RESET}\` to start the application
+- Run \`${COLOR_GREEN}python main.py${COLOR_RESET}\` to start the application
 "
 
 # Set terminal prompt
--- devspace/services/viewer-backend/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/shsma/container-tools/devspace/services/viewer-backend
-
-go 1.19
