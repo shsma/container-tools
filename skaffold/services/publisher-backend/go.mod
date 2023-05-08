316455169be3cb5d71d8af4f9326625f4d0dd00d
Shady Smaoui <shadysmaoui@hotmail.fr>
2023-05-08 00:21:52 -0400

    add skaffold publisher-backend

---
+++ skaffold/services/publisher-backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/shsma/container-tools/devspace/services/publisher-backend
+
+go 1.19
---
+++ skaffold/services/publisher-backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hellow World From publish-backend!")
+}
+
+func main() {
+	fmt.Println("Server Started on port: 9004")
+
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":9004", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
